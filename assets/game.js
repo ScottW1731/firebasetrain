7081637084e8b1ec821deeadcaca758f45ba7316
Your Name <you@example.com>
2018-10-30 13:19:20 -0500

    init firebase

---
+++ assets/game.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAaMaP7NiyLupDich-gzA-LWQF08HUlde0",
+    authDomain: "trainactivitybasic-7d39e.firebaseapp.com",
+    databaseURL: "https://trainactivitybasic-7d39e.firebaseio.com",
+    projectId: "trainactivitybasic-7d39e",
+    storageBucket: "trainactivitybasic-7d39e.appspot.com",
+    messagingSenderId: "177525492048"
+  };
+  firebase.initializeApp(config);
