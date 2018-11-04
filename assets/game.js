d46653da8706a7eb39feb0e741fed2fc4575d85c
Your Name <you@example.com>
2018-11-03 23:10:04 -0500

    comments and some bugfixes

--- assets/game.js
+++ assets/game.js
@@ -22,5 +22,12 @@ var config = {
   var arrivaltime = 0;
   var frequency = "";
 
-
+      // something I found on fire base youtube channel that I hope will help me
+      // write to my database channel
+    var bigone = document.getElementById('bigone');
+    var dbRef = firebase.database().ref().child('text');
+    dbRef.on('value', snap => bigone.innerText = snap.val());
+    // I could not figure out how to write to my database
+    // Do I have to put it in manually?
+    
 });
